ccd43b6969ada5203f8f606223ac106c8c7eb16d
mdsrmed <mdsrmed@gmail.com>
2023-08-31 22:37:06 -0400

    Initial Commit

---
+++ Deli-Newyork/Deli_NewyorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deli_NewyorkApp.swift
+//  Deli-Newyork
+//
+//  Created by Md Shohidur Rahman on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Deli_NewyorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
